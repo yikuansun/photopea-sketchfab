3d73fbab0377d5ef0657495ff8323c2a894ca6e9
Yikuan Sun <yikuansun2015@gmail.com>
2024-06-29 13:21:56 -0400

    Implement viewer api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from "svelte";
+
+    let viewerFrame;
+    let uid = "7w7pAfrCfjovwykkEeRFLGw5SXS";
+    let screenshotSrc = "";
+    let api;
+
+    onMount(() => {
+        let client = new Sketchfab(viewerFrame);
+        client.init(uid, {
+            success: (api_) => {
+                api = api_;
+                api.start();
+                api.addEventListener("viewerready", function() {
+                    console.log("viewer is ready");
+                });
+            },
+            error: () => {
+                console.log("error making viewer");
+            },
+            transparent: 1, // requires pro acc
+        });
+    });
+</script>
+
+<iframe src="" bind:this={viewerFrame} id="viewerFrame" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" title="sketchfab embed"></iframe>
+
+<br />
+<button on:click={() => {
+    api.getScreenShot("image/png", (err, result) => {
+        if (!err) {
+            screenshotSrc = result;
+        }
+    });
+}}>
+    Take screenshot
+</button>
+<br />
+
+<img src={screenshotSrc} alt="screenshot" />
+
+<style>
+    #viewerFrame {
+        width: 500px;
+        height: 500px;
+        border: 0;
+    }
+</style>
